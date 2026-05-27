bb880cfc551308aa671e2052378d825b454a43c2
Davi Samora <samora.davi@hotmail.com>
2026-05-27 15:43:39 -0300

    Add multi-model research file

---
+++ multi-models/multimodels/__init__.py
@@ -0,0 +1,5 @@
+"""Isolated modular research stack for Jane Street experiments."""
+
+from multimodels.pipeline import ExperimentConfig, run_experiment
+
+__all__ = ["ExperimentConfig", "run_experiment"]
